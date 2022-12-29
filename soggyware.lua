2ce72b3edc90f51305718c101f7fe12afcb429fd
Valco <80554644+RiseValco@users.noreply.github.com>
2022-12-29 14:32:59 -0500

    Create soggyware.lua

---
+++ soggyware.lua
@@ -0,0 +1,26 @@
+local old
+old = hookfunction(getgc, function(...)
+    print(...)
+   return {function() end} 
+end)
+
+local key = game:HttpGet("https://soggyware-bypass.vercel.app/api")
+
+local hooks = {
+    rconsoleprint = "dsc.gg/grimcity"
+}
+
+hooks.rconsoleprint = hookfunction(rconsoleprint, function(...)
+    return print(...)
+end)
+
+loadstring(game:HttpGet("https://soggy-ware.cf/"))()
+
+for _,v in pairs(game:GetService("CoreGui"):GetChildren()) do
+    if v:FindFirstChild("KeyUI") then
+    v.KeyUI:GetPropertyChangedSignal("Visible"):Connect(function()
+        wait(1)
+        v.KeyUI.TextBox.Text = getgenv().key
+    end)
+    end
+end
