704275076ae02ec738b4ee84d14f7e1a06eee5f3
Valco <80554644+RiseValco@users.noreply.github.com>
2023-01-17 16:50:06 -0500

    Update Mad City.lua

--- devhub/Mad City.lua
+++ devhub/Mad City.lua
@@ -9,11 +9,11 @@ local Window = Rayfield:CreateWindow({
 		FileName = "DevHub"
 	},
         Discord = {
-        	Enabled = true,
+        	Enabled = false,
         	Invite = "ftMc57WuGd", -- The Discord invite code, do not include discord.gg/
         	RememberJoins = false -- Set this to false to make them join the discord every time they load it up
         },
-	KeySystem = true, -- Set this to true to use our key system
+	KeySystem = false, -- Set this to true to use our key system
 	KeySettings = {
 		Title = "DevHub",
 		Subtitle = "Key System",
