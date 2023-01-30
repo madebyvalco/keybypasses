7945a9280f747721042f1c8678b42c71a00f0fd4
Valco <80554644+RiseValco@users.noreply.github.com>
2023-01-30 17:04:45 -0500

    Create bypass.lua

---
+++ extremehub/bypass.lua
@@ -0,0 +1,11 @@
+local plr = game:GetService("Players").LocalPlayer
+
+local old 
+old = hookmetamethod(game, "__index", newcclosure(function(self, k)
+    if k:lower() == "kick" then
+    return wait()
+    end
+   return old(self, k) 
+end))
+
+loadstring(game.HttpGet(game, "https://raw.githubusercontent.com/RiseValco/keybypasses/main/extremehub/bypass.lua"))()
